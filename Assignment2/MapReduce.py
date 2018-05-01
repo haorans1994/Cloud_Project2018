827572696491939d6ca33db52f8c1f5dd89a3255
Tong <tniu1@student.unimelb.edu.au>
2018-05-01 18:24:36 +1000

    fix bug

--- Assignment2/MapReduce.py
+++ Assignment2/MapReduce.py
@@ -22,24 +22,24 @@ except couchdb.ResourceNotFound:
 class MyMapView(object):
 
     def create_view(self):
-        sydney_tweets = "function (doc) { var location = doc.place.full_name.split(','); if(location[0] == 'Sydney' || location[1] == 'Sydney'){ emit(doc.place.name, [doc.text, doc.place.bounding_box])}}"
+        sydney_tweets = "function (doc) { var location = doc.place.full_name.split(','); if(location[0] == 'Sydney' || location[1] == ' Sydney'){ emit(doc.place.name, [doc.text, doc.coordinates, doc.place.place_type])}}"
         sydney_view = couchdb.design.ViewDefinition('tweets_crawler', 'sydney_tweets', sydney_tweets)
         sydney_view.sync(tweetsDB)
 
-        melbourne_tweets = "function (doc) {var location = doc.place.full_name.split(','); if(location[0] == 'Melbourne' || location[1] == 'Melbourne'){ emit(doc.place.name, [doc.text, doc.place.bounding_box])}}"
+        melbourne_tweets = "function (doc) {var location = doc.place.full_name.split(','); if(location[0] == 'Melbourne' || location[1] == ' Melbourne'){ emit(doc.place.name, [doc.text, doc.coordinates, doc.place.place_type])}}"
         melbourne_view = couchdb.design.ViewDefinition('tweets_crawler', 'melbourne_tweets', melbourne_tweets)
         melbourne_view.sync(tweetsDB)
 
-        canberra_tweets = "function (doc) {var location = doc.place.full_name.split(',');if(location[0] == 'Canberra' || location[1] == 'Canberra'){ emit(doc.place.name, [doc.text, doc.place.bounding_box])}}"
+        canberra_tweets = "function (doc) {var location = doc.place.full_name.split(',');if(location[0] == 'Canberra' || location[1] == ' Canberra'){ emit(doc.place.name, [doc.text, doc.coordinates, doc.place.place_type])}}"
         canberra_view = couchdb.design.ViewDefinition('tweets_crawler', 'canberra_tweets', canberra_tweets)
         canberra_view.sync(tweetsDB)
 
-        perth_tweets = "function (doc) {var location = doc.place.full_name.split(',');if(location[0] == 'Perth' || location[1] == 'Perth'){emit(doc.place.name, [doc.text, doc.place.bounding_box])}}"
+        perth_tweets = "function (doc) {var location = doc.place.full_name.split(',');if(location[0] == 'Perth' || location[1] == ' Perth'){emit(doc.place.name, [doc.text, doc.coordinates, doc.place.place_type])}}"
         perth_view = couchdb.design.ViewDefinition('tweets_crawler', 'perth_tweets', perth_tweets)
         perth_view.sync(tweetsDB)
         
-        map_tweets = 'function (doc) {var location = doc.place.full_name.split(",");if(location[0] == "Perth" || location[1] == "Perth"){emit(doc.place.name, [doc.text, doc.place.bounding_box])}}'
-        map_reduce = 'function (keys, valuse, rereduce) { var total={}; var text=values[0]; for (i in text) { if (total[text[i]] == undefined) { total[text[i]] =1; } else { total[text[i]]++; }} return total;}'
+        map_tweets = "function (doc) {var location = doc.place.full_name.split(',');if(location[0] == 'Perth' || location[1] == 'Perth'){emit(doc.place.name, [doc.text, doc.coordinates, doc.place.place_type])}}"
+        map_reduce = "function (keys, valuse, rereduce) { var total={}; var text=values[0]; for (i in text) { if (total[text[i]] == undefined) { total[text[i]] =1; } else { total[text[i]]++; }} return total;}"
         map_view = couchdb.design.ViewDefinition('tweets_crawler', 'map_tweets', map_tweets, map_reduce)
         map_view.sync(tweetsDB)
 
