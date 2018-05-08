d926268a4c4896e12ded1e33b6fbf280e65e9cea
Tong <tniu1@student.unimelb.edu.au>
2018-05-09 02:06:35 +1000

    fix

--- Assignment2/LGA/generateLGACode.py
+++ Assignment2/LGA/generateLGACode.py
@@ -3,7 +3,7 @@ import json
 codeLGA = 'LGA_code.json'
 # put a postcode to judge if it in certain LGA area and then return LGA_code
 def generateLGA_code(postcode):
-	file = open(codeLGA).read()
+	file = open(codeLGA)
 	fileJson = json.load(file)
 
 	for k, v in fileJson.items():
