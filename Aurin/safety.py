cc488302517cd51130769fa8530a9ef5630e5c01
tengfei <teng.tf27@gmail.com>
2018-05-08 23:55:31 +1000

    feat: Aurin data for four scenario

---
+++ Aurin/safety.py
@@ -0,0 +1,32 @@
+import json
+
+def safety(fileName):
+	file = open(fileName, 'rb')
+	fileJson = json.load(file)['features']
+	result = []
+
+	for item in fileJson:
+		item = item['properties']
+		line = {}
+		line['LGA_code'] = item['area_code']
+		line['LGA_name'] = item['area_name']
+		line['distressDate'] = item['k10_me_2_rate_3_11_7_13']
+		result.append(line)
+	return result
+
+def outputJson():
+	with open('final_distress_perth.json', 'w') as f:
+		json.dump(safety(fileName), f)
+		print 'completed'
+
+
+
+
+
+
+
+
+
+fileName = 'distress_perth.json'
+# print safety(fileName)
+outputJson()
