7005f53a85273333cd3b4f6a3fb964d88459d7dd
Tong <tniu1@student.unimelb.edu.au>
2018-05-09 15:47:13 +1000

    scenario 1

--- Assignment2/setPostCode.py
+++ Assignment2/setPostCode.py
@@ -49,6 +49,5 @@ for tweet in tweetsSearchDB.view('tweets_search/melbourne_tweets'):
                     lgaCode = lga['properties']['area_code']
                     break
     result = {'text' : tweet.value[0], 'sentiment': tweet.value[1], 'LgaCode': lgaCode, 'postcode': postcode, 'place_name': tweet.key}
-    # strs = json.dumps(result)
-    # tweetsMelbourne.save(json.loads(strs))
-    print(result)
+    strs = json.dumps(result)
+    tweetsMelbourne.save(json.loads(strs))
