df3ade1bd01ac2948ad30b61167490da94312313
GordonT0110 <30914240+GordonT0110@users.noreply.github.com>
2018-05-10 18:10:53 +1000

    Update Scratch.py

--- Assignment2/Scratch.py
+++ Assignment2/Scratch.py
@@ -10,7 +10,7 @@ from tweepy.utils import import_simplejson
 AUS_GEO_CODE = [113.03, -39.06, 154.73, -12.28]
 USER_NAME = "assignment2"
 PASSWORD = "3010"
-HOST_NAME = "115.146.86.176"
+HOST_NAME = "115.146.86.192"
 
 
 try:
